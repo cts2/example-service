0ec3f8a1f5cbfdb5485a2c7dff83688acb775d98
Kevin Peterson <kevin.peterson@mayo.edu>
2011-10-25 17:10:59 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+example-service - An Example CTS2 Development Framework Service Plugin
+======================
+
+example-service is a demonstration Service Plugin for the CTS2 Development Framework.
+
+Plugin Tutorial
+==================================
+
+This plugin is used in the tutorial http://informatics.mayo.edu/cts2/framework/example-plugin-tutorial/
+
+
+What It Includes
+==============================
+
+* An example ServiceProvider implementation
+* An example CodeSystemCatalogRead Service implementation
---
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/example/ExampleCodeSystemCatalogReadService.java
@@ -0,0 +1,43 @@
+package edu.mayo.cts2.framework.plugin.service.example;
+
+import org.apache.commons.lang.StringUtils;
+
+import edu.mayo.cts2.framework.model.codesystem.CodeSystemCatalogEntry;
+import edu.mayo.cts2.framework.model.core.EntryDescription;
+import edu.mayo.cts2.framework.model.service.core.NameOrURI;
+import edu.mayo.cts2.framework.model.service.core.ReadContext;
+import edu.mayo.cts2.framework.model.util.ModelUtils;
+import edu.mayo.cts2.framework.service.profile.codesystem.CodeSystemReadService;
+
+public class ExampleCodeSystemCatalogReadService implements CodeSystemReadService {
+
+	public boolean exists(NameOrURI nameOrUri, ReadContext readContext) {
+		//in this example, 'exists' is always true
+		return true;
+	}
+
+	public CodeSystemCatalogEntry read(NameOrURI nameOrUri, ReadContext readContext) {
+		//NameOrURI can either be a CodeSystenName or its URI. For this example,
+		//assume the request is 'by name'.
+		String codeSystemName = nameOrUri.getName();
+		
+		if(StringUtils.isBlank(codeSystemName)){
+			throw new UnsupportedOperationException("Only resolution by name is supported.");
+		}
+		
+		//In real-world scenarios, we would fetch from a database or other store. Here
+		//we just make up a Resource and return it back.
+		CodeSystemCatalogEntry entry = new CodeSystemCatalogEntry();
+		
+		entry.setCodeSystemName(codeSystemName);
+		entry.setAbout("http://cts2/demo#" + codeSystemName);
+		
+		EntryDescription ed = new EntryDescription();
+		ed.setValue(ModelUtils.toTsAnyType(
+				"The Code System: " + codeSystemName));
+		
+		entry.setResourceSynopsis(ed);
+			
+		return entry;
+	}
+}
